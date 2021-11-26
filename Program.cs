8a70cb833509e32cbed9079f1f8967dededf3162
Magnus Vahlström <Wahlstrommm@gmail.com>
2021-11-26 23:55:57 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace uppgift2_Avrundaren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej! Var vänlig och skriv in ett tal med minst fem decimaler!"); //Ber användaren skriva in ett tal med minst 5 decimaler.
+            double x = Convert.ToDouble(Console.ReadLine()); // Konverterar string till en double.
+            Console.WriteLine("Hur många decimaler vill du ha?"); //Ber användaren skriva in hur många decimaler hen vill ha. (notera att den kommer avrundas)
+            int y = Convert.ToInt32(Console.ReadLine()); // konverterar string till ett heltal innehåller antal decimaler.
+            Console.WriteLine(Math.Round(x,y));//Avrundar och skriver ut talet.
+            Console.ReadKey();// så programmet inte stängs ner.
+        }
+    }
+}
